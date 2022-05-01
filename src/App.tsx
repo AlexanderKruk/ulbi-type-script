1227b55cfac3e7026dbd72151d6ae79302f5cd5d
Alexander Kruk <alexander.kruk@gmail.com>
2022-05-01 14:36:38 +0200

    added List

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,31 @@
+import axios from 'axios';
+import { useState, useEffect } from 'react';
 import { Card, CardVariant } from "./component/Card";
-import { UserList } from "./component/UserList";
+import List from './component/List';
+import UserItem from './component/UserItem';
 import { IUser } from "./types/types";
 
 function App() {
 
-  const users: IUser[] = [
-    { id: 1 , name: 'Alex', email: '1@1.by', address: { street: 'Victory', city: 'Wroclaw', zipcode: 14123 }},
-    { id: 2, name: 'Victor', email: '1@2.by', address: {street: 'Wisdom', city: 'Warszawa', zipcode: 12452 }}
-  ]
+  // const users: IUser[] = [
+  //   { id: 1 , name: 'Alex', email: '1@1.by', address: { street: 'Victory', city: 'Wroclaw', zipcode: 14123 }},
+  //   { id: 2, name: 'Victor', email: '1@2.by', address: {street: 'Wisdom', city: 'Warszawa', zipcode: 12452 }}
+  // ]
+
+  const [users, setUsers] = useState<IUser[]>([])
+
+  useEffect(() => {
+    fetchUsers()
+  }, [])
+
+  async function fetchUsers() {
+    try {
+      const response = await axios.get<IUser[]>('https://jsonplaceholder.typicode.com/users?_limit=10')
+      setUsers(response.data)
+    } catch(e) {
+      console.log(e)
+    }
+  }
 
   return (
     <div>
@@ -18,7 +36,7 @@ function App() {
         onClick={(num) => console.log('number', num )}>
         <button>Test</button>
       </Card>
-      <UserList users={users}/>
+      <List items={users} renderItem={(user: IUser) => <UserItem key={user.id} user={user}/>}/>
     </div>
   )
 }
